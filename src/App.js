197e25cfeea29d10d58abaf27ecf07ac1f4771eb
Mohanish9398 <mohanishvellore34@gmail.com>
2026-03-12 11:21:34 +0530

    enhanced my UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+
 import Home from './pages/Home'
 import Coursedetails from './pages/Coursedetails'
 import Mycourses from './pages/Mycourses'
@@ -13,6 +15,31 @@ import Landing from './pages/Landing'
 import Admin from './pages/Admin'
 
 function App() {
+
+  useEffect(() => {
+
+    const handleMouseMove = (e) => {
+      const cards = document.querySelectorAll('.course-card')
+
+      cards.forEach(card => {
+        const rect = card.getBoundingClientRect()
+
+        const x = e.clientX - rect.left
+        const y = e.clientY - rect.top
+
+        card.style.setProperty('--x', `${x}px`)
+        card.style.setProperty('--y', `${y}px`)
+      })
+    }
+
+    window.addEventListener('mousemove', handleMouseMove)
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove)
+    }
+
+  }, [])
+
   return (
     <Router>
       <Navbar />
